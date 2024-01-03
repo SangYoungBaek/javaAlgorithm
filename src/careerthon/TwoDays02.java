5e5a6ee280c32ee33bc75ed3907f5c003d240719
SangYoungBaek <bsy2274@naver.com>
2024-01-03 15:36:42 +0900

    0103 commit ( 커리어톤 2일차 3, 4번 문제 commit )

---
+++ src/careerthon/TwoDays02.java
@@ -0,0 +1,23 @@
+package careerthon;
+
+public class TwoDays02 {
+    public String solution(String[] seoul) {
+        int answer = -1;
+        int start = 0;
+        int end = seoul.length - 1;
+
+        while (start < end) {
+            if(seoul[start].equals("Kim")){
+                answer = start;
+                break;
+            }
+            if(seoul[end].equals("Kim")){
+                answer = end;
+                break;
+            }
+            start++;
+            end--;
+        }
+        return String.format("김서방은 %d에 있다", answer);
+    }
+}
