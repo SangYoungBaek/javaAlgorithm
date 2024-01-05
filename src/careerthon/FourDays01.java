0e14d92e107fc5532b42f565312df51ba1c6d778
SangYoungBaek <bsy2274@naver.com>
2024-01-05 15:30:12 +0900

    0105 commit ( 커리어톤 4일차 7, 8번 문제 commit )

---
+++ src/careerthon/FourDays01.java
@@ -0,0 +1,29 @@
+package careerthon;
+
+import java.util.Stack;
+
+public class FourDays01 {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+
+        Stack<Integer> inStack = new Stack<Integer>();
+
+        for(int in : ingredient){
+            inStack.push(in);
+
+            if(inStack.size() >= 4) {
+
+                if(inStack.get(inStack.size()-4) == 1
+                        && inStack.get(inStack.size()-3) == 2
+                        && inStack.get(inStack.size()-2) == 3
+                        && inStack.get(inStack.size()-1) == 1) {
+
+                    answer++;
+                    inStack.pop(); inStack.pop(); inStack.pop(); inStack.pop();
+                }
+            }
+        }
+
+        return answer;
+    }
+}
