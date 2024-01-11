b3597c1e92ca328ed4301cfd82513bd7a13fe56c
SangYoungBaek <bsy2274@naver.com>
2024-01-11 16:40:58 +0900

    0111 commit

---
+++ src/careerthon/Days0111_01.java
@@ -0,0 +1,16 @@
+package careerthon;
+
+public class Days0111_01 {
+    //멀리 뛰
+    public long solution(int n) {
+        long answer = 0;
+        int[] dp = new int[2001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<2001; i++){
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        answer = dp[n];
+        return answer;
+    }
+}
